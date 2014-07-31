822d26b625f78f90d5ae9cc345ef5a74059eecc4
c9s <yoanlin93@gmail.com>
2014-07-31 21:06:41 +0800

    Add gtest

---
+++ gtest-1.7.0/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/c9s/work/a3/gtest-1.7.0
+# Build directory: /Users/c9s/work/a3/gtest-1.7.0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
