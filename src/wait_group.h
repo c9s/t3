402e532e53dc31b8857a84dcbc7bfe235e686df9
c9s <yoanlin93@gmail.com>
2014-08-03 21:53:19 +0800

    Add wait_group.h

---
+++ src/wait_group.h
@@ -0,0 +1,19 @@
+/*
+ * wait_group.h
+ * Copyright (C) 2014 c9s <c9s@c9smba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef WAIT_GROUP_H
+#define WAIT_GROUP_H
+
+#include <atomic>
+
+namespace t3 {
+
+
+};
+
+
+#endif /* !WAIT_GROUP_H */
