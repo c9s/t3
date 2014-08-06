aa441475f680be57df976fa4984557cec9b5ef8e
c9s <yoanlin93@gmail.com>
2014-08-06 20:44:08 +0800

    update synopsis

--- src/progress_reporter.cc
+++ src/progress_reporter.cc
@@ -7,3 +7,17 @@
 #include "progress_reporter.h"
 
 
+/**
+ * bool displayHandler(float finished, float total) {
+ *
+ *      return false; // stop the progress reporter.
+ * }
+ *
+ *
+ * ProgressReporter reporter(&task, displayHandler);
+ *
+ * the progress reporter watches & waits on the mutex of the task and return
+ * the progress value to the display handler
+ */
+
+
