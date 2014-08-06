eeb06f1416cfc1188e46e8ad5878f2f5dd17bcb6
c9s <yoanlin93@gmail.com>
2014-08-06 16:11:06 +0800

    Add t3.h

---
+++ src/t3.h
@@ -0,0 +1,15 @@
+/*
+ * t3.h
+ * Copyright (C) 2014 c9s <c9s@c9smba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef T3_H
+#define T3_H
+
+#include "thread_task.h"
+#include "thread_pool.h"
+#include "channel.h"
+
+#endif /* !T3_H */
